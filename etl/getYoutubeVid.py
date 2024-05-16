cb8c74668d16b58dbe418bb945649413f1f371bd
nitheesh <nitheeshkoushikgattu@Nitheeshs-MacBook-Air.local>
2024-05-16 13:07:43 -0400

    Loading the results in BigQ

--- etl/getYoutubeVid.py
+++ etl/getYoutubeVid.py
@@ -1,4 +1,3 @@
-from datetime import datetime, timedelta
 import json
 import googleapiclient.discovery
 from google.cloud import secretmanager, bigquery
@@ -21,25 +20,49 @@ def getVideos(query, apiKey):
     youtube = googleapiclient.discovery.build(
 		"youtube", "v3", developerKey=apiKey
 	)
-    current_time = datetime.now() 
-
-    # Convert the current time to a string in ISO 8601 format with 'Z' at the end
-    # published_after = current_time.strftime('%Y-%m-%dT%H:%M:%SZ')
     search_response = (
     youtube.search()
     .list(
         part = "snippet",
-        maxResults=10,  # Specify the maximum number of results to retrieve
-        q=query,
-        # publishedAfter=published_after, 
-        order="viewCount"
+        maxResults=10,  
+        q=query
     )
     .execute())
-    return search_response
+    df = pd.json_normalize(search_response["items"])
+    df = df[["id.videoId", "snippet.title", "snippet.thumbnails.medium.url"]]
+    df.rename({'id.videoId' : 'videoID',
+               'snippet.title': 'title',
+               'snippet.thumbnails.medium.url': 'url'}, axis = 1, inplace = True)
+    k = query.replace(" Podcast", "")
+    df["league"] = k
+    return df
+
+
+def loadData(df, dataset, table):
+    client = bigquery.Client()
+    dataset_ref = client.dataset(dataset)
+    table_ref = dataset_ref.table(table)
+    job_config = bigquery.LoadJobConfig()
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
+    job = client.load_table_from_dataframe(
+        df, table_ref, job_config=job_config
+    )
+
+    job.result()
+    return None
+
 
 if __name__ == "__main__":
+    leagues = ["Indian Premier League", "T20 Blast UK", "Big Bash T20 Australia", 
+               "Pakistan Super League", "Bangladesh Premier League", "CSA T20 Challenge",
+               "Sri Lanka Premier League", "New Zealand Super Smash", "Caribbean Premier League", ]
+    
     apikey= getYoutubeKey()
-    print(datetime.now())
-    result = getVideos("IPL Podcast", apikey)
-    with open("youtube.json", "w") as f:
-        json.dump(result, f, indent=4)
+    for i, league in enumerate(leagues):
+        if i == 0:
+            masterDF = getVideos(league + " Podcast", apikey)
+        else:
+            articles = getVideos(league +  " Podcast", apikey)
+            masterDF = pd.concat([masterDF, articles],ignore_index= True)
+    loadData(masterDF, "fcDataSets", "podcasts")
+    
