a61db2d4657905b9ed1e058d44a7beb1a75626dc
nitheesh <nitheeshkoushikgattu@Nitheeshs-MacBook-Air.local>
2024-05-16 18:43:34 -0400

    Added Home page

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+if __name__ == '__main__':
+    st.title("All things Franchise Cricket")
+    st.header("About") 
+    
+    st.write("As a big fan of Franchise Cricket, it's always bothered me how much attention the Indian Premier League (IPL) gets compared to other leagues.")
+    st.write("So, I've created this web app to blend my passion for Cricket with Data Engineering.")
+    st.write("Here, you can stay updated on news, podcasts, standings, squads, and stats from Franchise Cricket leagues around the world.")
+    st.write("Plus, building this app has helped me learn some Data Engineering tools and techniques along the way.")
+
